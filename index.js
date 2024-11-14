cbe35adb0575ebb6c2eba609d528f406615d8143
Ashad Jamal <ashadjamalhyt@gmail.com>
2024-11-14 23:55:43 +0530

    adding cors

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(
   cors({
     origin: "*",
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
     credentials: true,
   })
 );
