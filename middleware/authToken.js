330b0fa32f7c0b1deea6fca15325e615aaf0755f
Ashad Jamal <ashadjamalhyt@gmail.com>
2024-11-18 00:39:41 +0530

    store token in localstorage

--- middleware/authToken.js
+++ middleware/authToken.js
@@ -3,9 +3,10 @@ const httpStatusCode = require("../constants/httpStatusCode");
 
 async function authToken(req, res, next) {
   try {
-    const token = req.cookies?.token;
+    // const token = req.cookies?.token;
+    const token = req.headers.authorization.split(" ")[1];
     console.log("token--", token);
-    
+
     if (!token) {
       return res.status(httpStatusCode.Ok).json({
         message: "Please Login...!",
@@ -13,16 +14,16 @@ async function authToken(req, res, next) {
         success: false,
       });
     }
-    jwt.verify(token, process.env.SECRET_KEY, function (error,decoded){
-        console.log(error);
-        console.log("decoded---",decoded);
+    jwt.verify(token, process.env.SECRET_KEY, function (error, decoded) {
+      console.log(error);
+      console.log("decoded---", decoded);
 
-        if(error){
-            console.log("error auth-", error);
-        }
-        req.userId = decoded?._id;
-        next();
-    })
+      if (error) {
+        console.log("error auth-", error);
+      }
+      req.userId = decoded?._id;
+      next();
+    });
   } catch (error) {
     res.status(httpStatusCode.BAD_REQUEST).json({
       message: error.message || error,
